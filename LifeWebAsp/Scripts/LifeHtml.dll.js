c5b7137b9b52ba2cc0067535b2693db1be589851
Paul Blair <ciriwe@phobot.net>
2012-10-30 16:45:01 -0400

    Set main branch back to the latest commit after reverting for the purposes of building 0.9.0 on AppHarbor. This reverts commit 4163d63717912dc2d4313563cfc65ba4df46832c.

--- LifeWebAsp/Scripts/LifeHtml.dll.js
+++ LifeWebAsp/Scripts/LifeHtml.dll.js
@@ -1,6 +1,6 @@
 (function()
 {
- var Global=this,Runtime=this.IntelliFactory.Runtime,WebSharper,Control,_FSharpEvent_1,Life,Game,LifeHtml,Canvas,Number,Seq,Collections,BalancedTree,_FSharpSet_1,SetModule,Html,Default,HTML5,List,T,Unchecked,G_vmlCanvasManager,Util,Operators;
+ var Global=this,Runtime=this.IntelliFactory.Runtime,WebSharper,Control,_FSharpEvent_1,LifeHtml,Canvas,Life,Game,Number,Seq,Collections,BalancedTree,_FSharpSet_1,SetModule,Html,Default,HTML5,List,T,Unchecked,G_vmlCanvasManager,Util,Operators;
  Runtime.Define(Global,{
   LifeHtml:{
    Canvas:{
@@ -8,14 +8,14 @@
     {
      return _FSharpEvent_1.New();
     }),
-    adjustInitArrayAndRedraw:function(context,width,height,cellSide,canvasXOffset,canvasYOffset,evt)
+    adjustInitArrayAndRedraw:function(evt)
     {
      var x,y;
-     x=(evt.clientX-canvasXOffset)/cellSide>>0;
-     y=(evt.clientY-canvasYOffset)/cellSide>>0;
+     x=(evt.clientX-Canvas.canvasXOffset())/Canvas.cellSide()>>0;
+     y=(evt.clientY-Canvas.canvasYOffset())/Canvas.cellSide()>>0;
      return Game.toggleCellAndRedraw(x,y,function(gameState)
      {
-      return Canvas.drawLife(context,width,height,cellSide,gameState);
+      return Canvas.drawLife(gameState);
      });
     },
     canvas:Runtime.Field(function()
@@ -24,82 +24,94 @@
      _this=Canvas.element();
      return _this.Body;
     }),
-    cellIsWithinViewableCanvas:function(_,_1,_2,_3)
+    canvasXOffset:Runtime.Field(function()
     {
-     return(Runtime.Tupled(function(canvasCoordinatePair)
-     {
-      if((canvasCoordinatePair[0]>0?canvasCoordinatePair[0]<Number(_):false)?canvasCoordinatePair[1]>0:false)
-       {
-        return canvasCoordinatePair[1]<Number(_1);
-       }
-      else
-       {
-        return false;
-       }
-     }))([_2,_3]);
+     return 0;
+    }),
+    canvasYOffset:Runtime.Field(function()
+    {
+     return 0;
+    }),
+    cellIsWithinViewableCanvas:function(_,_1)
+    {
+     var canvasCoordinatePair;
+     canvasCoordinatePair=[_,_1];
+     if((canvasCoordinatePair[0]>0?canvasCoordinatePair[0]<Number(Canvas.canvas().width):false)?canvasCoordinatePair[1]>0:false)
+      {
+       return canvasCoordinatePair[1]<Number(Canvas.canvas().height);
+      }
+     else
+      {
+       return false;
+      }
     },
+    cellSide:Runtime.Field(function()
+    {
+     return 0;
+    }),
     context:Runtime.Field(function()
     {
      return Canvas.canvas().getContext("2d");
     }),
-    drawLife:function(context,width,height,cellSide,gameState)
+    drawLife:function(gameState)
     {
-     var x,x1,f,mapping,f1,predicate,f2,action;
-     context.save();
-     context.clearRect(0,0,Number(width),Number(height));
-     context.fillStyle="#8EE5EE";
-     context.fillRect(0,0,Number(width),Number(height));
-     context.save();
-     context.strokeStyle="black";
-     context.fillStyle="black";
-     context.lineWidth=1;
-     x=(x1=(f=(mapping=Runtime.Tupled(function(tupledArg)
+     var x,x1,f,f2,f3;
+     Canvas.context().save();
+     Canvas.context().clearRect(0,0,Number(Canvas.canvas().width),Number(Canvas.canvas().height));
+     Canvas.context().fillStyle="#8EE5EE";
+     Canvas.context().fillRect(0,0,Number(Canvas.canvas().width),Number(Canvas.canvas().height));
+     Canvas.context().save();
+     Canvas.context().strokeStyle="black";
+     Canvas.context().fillStyle="black";
+     Canvas.context().lineWidth=1;
+     x=(x1=(f=function(set)
      {
-      var _arg10_,_arg11_;
-      _arg10_=tupledArg[0];
-      _arg11_=tupledArg[1];
-      return Canvas.transformCellCoordinatesToCanvasCoordinates(cellSide,_arg10_,_arg11_);
-     }),function(set)
-     {
-      var a,t;
-      a=Seq.map(mapping,set);
+      var f1,a,t;
+      f1=Runtime.Tupled(function(tupledArg)
+      {
+       var _arg00_,_arg01_;
+       _arg00_=tupledArg[0];
+       _arg01_=tupledArg[1];
+       return Canvas.transformCellCoordinatesToCanvasCoordinates(_arg00_,_arg01_);
+      });
+      a=Seq.map(f1,set);
       t=BalancedTree.OfSeq(a);
       return _FSharpSet_1.New1(t);
-     }),f(gameState)),(f1=(predicate=Runtime.Tupled(function(tupledArg)
+     },f(gameState)),(f2=function(set)
      {
-      var _arg20_,_arg21_;
-      _arg20_=tupledArg[0];
-      _arg21_=tupledArg[1];
-      return Canvas.cellIsWithinViewableCanvas(width,height,_arg20_,_arg21_);
-     }),function(set)
+      return SetModule.Filter(Runtime.Tupled(function(tupledArg)
+      {
+       var _arg00_,_arg01_;
+       _arg00_=tupledArg[0];
+       _arg01_=tupledArg[1];
+       return Canvas.cellIsWithinViewableCanvas(_arg00_,_arg01_);
+      }),set);
+     },f2(x1)));
+     f3=function(set)
      {
-      return SetModule.Filter(predicate,set);
-     }),f1(x1)));
-     f2=(action=Runtime.Tupled(function(tupledArg)
-     {
-      var _arg20_,_arg21_;
-      _arg20_=tupledArg[0];
-      _arg21_=tupledArg[1];
-      return Canvas.drawSmallCircleAtPoint(context,cellSide,_arg20_,_arg21_);
-     }),function(set)
-     {
-      return Seq.iter(action,set);
-     });
-     f2(x);
-     return context.save();
+      var f1;
+      f1=Runtime.Tupled(function(tupledArg)
+      {
+       var _arg00_,_arg01_;
+       _arg00_=tupledArg[0];
+       _arg01_=tupledArg[1];
+       return Canvas.drawSmallCircleAtPoint(_arg00_,_arg01_);
+      });
+      return Seq.iter(f1,set);
+     };
+     f3(x);
+     return Canvas.context().save();
     },
-    drawSmallCircleAtPoint:function(_,_1,_2,_3)
+    drawSmallCircleAtPoint:function(_,_1)
     {
-     return(Runtime.Tupled(function(canvasCoordinatePair)
-     {
-      var radius,endAngle;
-      radius=(Number(_1)-2)/2;
-      endAngle=3.14159265358979*2;
-      _.beginPath();
-      _.arc(canvasCoordinatePair[0],canvasCoordinatePair[1],radius,0,endAngle,true);
-      _.closePath();
-      return _.stroke();
-     }))([_2,_3]);
+     var canvasCoordinatePair,radius,endAngle;
+     canvasCoordinatePair=[_,_1];
+     radius=(Number(Canvas.cellSide())-2)/2;
+     endAngle=3.14159265358979*2;
+     Canvas.context().beginPath();
+     Canvas.context().arc(canvasCoordinatePair[0],canvasCoordinatePair[1],radius,0,endAngle,true);
+     Canvas.context().closePath();
+     return Canvas.context().stroke();
     },
     element:Runtime.Field(function()
     {
@@ -110,7 +122,7 @@
      });
      return _this.NewTag("canvas",x);
     }),
-    initialize:function(width,height,cellSide,canvasXOffset,canvasYOffset)
+    initialize:function(width,height,cellSidePixels,canvasXOffsetPixels,canvasYOffsetPixels)
     {
      var target,_this;
      if(Unchecked.Equals((target=Canvas.canvas(),target.getContext),undefined))
@@ -119,11 +131,23 @@
       }
      Canvas.canvas().width=width;
      Canvas.canvas().height=height;
+     Canvas.cellSide=function()
+     {
+      return cellSidePixels;
+     };
+     Canvas.canvasXOffset=function()
+     {
+      return canvasXOffsetPixels;
+     };
+     Canvas.canvasYOffset=function()
+     {
+      return canvasYOffsetPixels;
+     };
      Canvas.context().canvas.addEventListener("click",function(evt)
      {
-      return Canvas.adjustInitArrayAndRedraw(Canvas.context(),width,height,cellSide,canvasXOffset,canvasYOffset,evt);
+      return Canvas.adjustInitArrayAndRedraw(evt);
      },false);
-     Canvas.drawLife(Canvas.context(),width,height,cellSide,Game.currentState().contents);
+     Canvas.drawLife(Game.currentState().contents);
      return Util.addListener((_this=Canvas.GameEvents(),_this.event),function(_arg1)
      {
       if(_arg1.$==1)
@@ -136,14 +160,14 @@
          {
           return Game.stopDrawingAndReset(function(gameState)
           {
-           return Canvas.drawLife(Canvas.context(),width,height,cellSide,gameState);
+           return Canvas.drawLife(gameState);
           });
          }
         else
          {
           return Game.startDrawing(function(gameState)
           {
-           return Canvas.drawLife(Canvas.context(),width,height,cellSide,gameState);
+           return Canvas.drawLife(gameState);
           });
          }
        }
@@ -154,21 +178,19 @@
      var _this,_this1;
      return Operators.add(Default.Div(List.ofArray([Default.Width(Global.String(Canvas.canvas().width)),(_this=Default.Attr(),_this.NewAttr("style","float:left; clear:both"))])),List.ofArray([Operators.add(Default.Div(List.ofArray([(_this1=Default.Attr(),_this1.NewAttr("style","float:center"))])),List.ofArray([Canvas.element()]))]));
     },
-    transformCellCoordinatesToCanvasCoordinates:function(_,_1,_2)
+    transformCellCoordinatesToCanvasCoordinates:function(_,_1)
     {
-     return(Runtime.Tupled(function(cell)
+     var cell,centerX,x,f,centerY,x1,f1;
+     cell=[_,_1];
+     centerX=(x=cell[0]*Canvas.cellSide()+(Canvas.cellSide()/2>>0),(f=function(value)
      {
-      var centerX,x,f,centerY,x1,f1;
-      centerX=(x=cell[0]*_+(_/2>>0),(f=function(value)
-      {
-       return Number(value);
-      },f(x)));
-      centerY=(x1=cell[1]*_+(_/2>>0),(f1=function(value)
-      {
-       return Number(value);
-      },f1(x1)));
-      return[centerX,centerY];
-     }))([_1,_2]);
+      return Number(value);
+     },f(x)));
+     centerY=(x1=cell[1]*Canvas.cellSide()+(Canvas.cellSide()/2>>0),(f1=function(value)
+     {
+      return Number(value);
+     },f1(x1)));
+     return[centerX,centerY];
     }
    }
   }
@@ -178,10 +200,10 @@
   WebSharper=Runtime.Safe(Global.IntelliFactory.WebSharper);
   Control=Runtime.Safe(WebSharper.Control);
   _FSharpEvent_1=Runtime.Safe(Control["FSharpEvent`1"]);
-  Life=Runtime.Safe(Global.Life);
-  Game=Runtime.Safe(Life.Game);
   LifeHtml=Runtime.Safe(Global.LifeHtml);
   Canvas=Runtime.Safe(LifeHtml.Canvas);
+  Life=Runtime.Safe(Global.Life);
+  Game=Runtime.Safe(Life.Game);
   Number=Runtime.Safe(Global.Number);
   Seq=Runtime.Safe(WebSharper.Seq);
   Collections=Runtime.Safe(WebSharper.Collections);
@@ -202,6 +224,9 @@
  {
   Canvas.element();
   Canvas.context();
+  Canvas.cellSide();
+  Canvas.canvasYOffset();
+  Canvas.canvasXOffset();
   Canvas.canvas();
   Canvas.GameEvents();
  });
