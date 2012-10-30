4163d63717912dc2d4313563cfc65ba4df46832c
Paul Blair <ciriwe@phobot.net>
2012-10-30 16:39:13 -0400

    Revert "Store some state in the canvas instead of passing it all over the place. Coming soon: Change Canvas to a type so these can be immutable."
    This reverts commit 08f3b0367eb5c05e4a01fb32188b4867b0a48d78.

--- LifeHtml/Canvas.fs
+++ LifeHtml/Canvas.fs
@@ -7,6 +7,63 @@ open System
       open IntelliFactory.WebSharper.Html5
       open Life.Game
  
+      // Since IE does not support canvas natively. Initialization of the 
+      // canvas element is done through the excanvas.js library.
+      [<Inline "G_vmlCanvasManager.initElement($elem)">]
+      let initializeForIE6 (elem: CanvasElement) : unit = ()
+
+      type CanvasCoordinatePair = float * float
+
+      [<JavaScript>]
+      let transformCellCoordinatesToCanvasCoordinates cellSideInPixels cell : CanvasCoordinatePair = 
+         let centerX = ((fst cell) * cellSideInPixels) + (cellSideInPixels / 2) |> float
+         let centerY = ((snd cell) * cellSideInPixels) + (cellSideInPixels / 2) |> float
+         (centerX, centerY)
+
+      [<JavaScript>]
+      let cellIsWithinViewableCanvas width height canvasCoordinatePair =
+        (fst canvasCoordinatePair) > 0. && (fst canvasCoordinatePair) < (float width) &&
+            (snd canvasCoordinatePair) > 0. && (snd canvasCoordinatePair) < (float height)
+
+      [<JavaScript>]
+      let drawSmallCircleAtPoint (ctx:CanvasRenderingContext2D) cellSideInPixels canvasCoordinatePair =
+         let radius = ((float cellSideInPixels) - 2.0) / 2.0
+         let startAngle = 0.0
+         let endAngle = Math.PI * 2.
+         ctx.BeginPath()
+         ctx.Arc((fst canvasCoordinatePair), (snd canvasCoordinatePair), radius, startAngle, endAngle, true)
+         ctx.ClosePath()
+         ctx.Stroke()
+
+      [<JavaScript>]
+      let drawLife (context:CanvasRenderingContext2D) width height cellSide (gameState: Set<int*int>) =
+         context.Save()
+         context.ClearRect(0., 0., (float width), (float height))
+         context.FillStyle <- "#8EE5EE"
+         context.FillRect(0., 0., (float width), (float height))
+         context.Save()
+
+         context.StrokeStyle <- "black"
+         context.FillStyle <- "black"
+         context.LineWidth <- 1.
+
+         gameState |> Set.map (transformCellCoordinatesToCanvasCoordinates cellSide)
+                   |> Set.filter (cellIsWithinViewableCanvas width height) 
+                   |> Set.iter (drawSmallCircleAtPoint context cellSide)      
+         context.Save()
+
+      [<JavaScript>]
+      let adjustInitArrayAndRedraw (context:CanvasRenderingContext2D) width height cellSide canvasXOffset canvasYOffset (evt:Dom.Event) =
+         let event = evt :?> Dom.MouseEvent
+         let x = (event.ClientX - canvasXOffset) / cellSide
+         let y = (event.ClientY - canvasYOffset) / cellSide
+         toggleCellAndRedraw (x,y) (drawLife context width height cellSide)
+
+      type GameEvent = | Go | Stop | Reset
+     
+      [<JavaScript>]
+      let GameEvents = new Event<GameEvent>()
+ 
       [<JavaScript>]
       let element = HTML5.Tags.Canvas []
 
@@ -17,90 +74,21 @@ open System
       let context = canvas.GetContext "2d"
 
       [<JavaScript>]
-      let mutable cellSide = 0
-
-      [<JavaScript>]
-      let mutable canvasXOffset = 0
-
-      [<JavaScript>]
-      let mutable canvasYOffset = 0
-
-      // Since IE does not support canvas natively. Initialization of the 
-      // canvas element is done through the excanvas.js library.
-      [<Inline "G_vmlCanvasManager.initElement($elem)">]
-      let initializeForIE6 (elem: CanvasElement) : unit = ()
-
-      type CanvasCoordinatePair = float * float
-
-      [<JavaScript>]
-      let transformCellCoordinatesToCanvasCoordinates cell : CanvasCoordinatePair = 
-         let centerX = ((fst cell) * cellSide) + (cellSide / 2) |> float
-         let centerY = ((snd cell) * cellSide) + (cellSide / 2) |> float
-         (centerX, centerY)
-
-      [<JavaScript>]
-      let cellIsWithinViewableCanvas canvasCoordinatePair =
-        (fst canvasCoordinatePair) > 0. && (fst canvasCoordinatePair) < (float canvas.Width) &&
-            (snd canvasCoordinatePair) > 0. && (snd canvasCoordinatePair) < (float canvas.Height)
-
-      [<JavaScript>]
-      let drawSmallCircleAtPoint canvasCoordinatePair =
-         let radius = ((float cellSide) - 2.0) / 2.0
-         let startAngle = 0.0
-         let endAngle = Math.PI * 2.
-         context.BeginPath()
-         context.Arc((fst canvasCoordinatePair), (snd canvasCoordinatePair), radius, startAngle, endAngle, true)
-         context.ClosePath()
-         context.Stroke()
-
-      [<JavaScript>]
-      let drawLife (gameState: Set<int*int>) =
-         context.Save()
-         context.ClearRect(0., 0., (float canvas.Width), (float canvas.Height))
-         context.FillStyle <- "#8EE5EE"
-         context.FillRect(0., 0., (float canvas.Width), (float canvas.Height))
-         context.Save()
-
-         context.StrokeStyle <- "black"
-         context.FillStyle <- "black"
-         context.LineWidth <- 1.
-
-         gameState |> Set.map transformCellCoordinatesToCanvasCoordinates
-                   |> Set.filter cellIsWithinViewableCanvas 
-                   |> Set.iter drawSmallCircleAtPoint      
-         context.Save()
-
-      [<JavaScript>]
-      let adjustInitArrayAndRedraw (evt:Dom.Event) =
-         let event = evt :?> Dom.MouseEvent
-         let x = (event.ClientX - canvasXOffset) / cellSide
-         let y = (event.ClientY - canvasYOffset) / cellSide
-         toggleCellAndRedraw (x,y) drawLife
-
-      type GameEvent = | Go | Stop | Reset
-     
-      [<JavaScript>]
-      let GameEvents = new Event<GameEvent>()
-
-      [<JavaScript>]
-      let initialize width height cellSidePixels canvasXOffsetPixels canvasYOffsetPixels = 
+      let initialize (width:int) (height:int) (cellSide:int) (canvasXOffset:int) (canvasYOffset:int) = 
          // Conditional initialization for the case of IE.
          if (JavaScript.Get "getContext" canvas = JavaScript.Undefined) then
                initializeForIE6 canvas
             
          canvas.Width  <- width
          canvas.Height <- height
-         cellSide <- cellSidePixels
-         canvasXOffset <- canvasXOffsetPixels
-         canvasYOffset <- canvasYOffsetPixels
 
-         context.Canvas.AddEventListener("click", adjustInitArrayAndRedraw, false)
+         context.Canvas.AddEventListener("click", (adjustInitArrayAndRedraw context width height cellSide canvasXOffset canvasYOffset), false)
+          
+         drawLife context width height cellSide !currentState
 
-         drawLife !currentState
-
-         GameEvents.Publish.Add (function | Go -> startDrawing drawLife
+         GameEvents.Publish.Add (function | Go -> startDrawing (drawLife context width height cellSide) 
                                           | Stop -> stopDrawing ()
-                                          | Reset -> stopDrawingAndReset drawLife )
+                                          | Reset -> stopDrawingAndReset (drawLife context width height cellSide) )
 
       [<JavaScript>]
       let nodes () = 
--- LifeWebAsp/Scripts/LifeHtml.dll.js
+++ LifeWebAsp/Scripts/LifeHtml.dll.js
@@ -1,6 +1,6 @@
 (function()
 {
- var Global=this,Runtime=this.IntelliFactory.Runtime,WebSharper,Control,_FSharpEvent_1,LifeHtml,Canvas,Life,Game,Number,Seq,Collections,BalancedTree,_FSharpSet_1,SetModule,Html,Default,HTML5,List,T,Unchecked,G_vmlCanvasManager,Util,Operators;
+ var Global=this,Runtime=this.IntelliFactory.Runtime,WebSharper,Control,_FSharpEvent_1,Life,Game,LifeHtml,Canvas,Number,Seq,Collections,BalancedTree,_FSharpSet_1,SetModule,Html,Default,HTML5,List,T,Unchecked,G_vmlCanvasManager,Util,Operators;
  Runtime.Define(Global,{
   LifeHtml:{
    Canvas:{
@@ -8,14 +8,14 @@
     {
      return _FSharpEvent_1.New();
     }),
-    adjustInitArrayAndRedraw:function(evt)
+    adjustInitArrayAndRedraw:function(context,width,height,cellSide,canvasXOffset,canvasYOffset,evt)
     {
      var x,y;
-     x=(evt.clientX-Canvas.canvasXOffset())/Canvas.cellSide()>>0;
-     y=(evt.clientY-Canvas.canvasYOffset())/Canvas.cellSide()>>0;
+     x=(evt.clientX-canvasXOffset)/cellSide>>0;
+     y=(evt.clientY-canvasYOffset)/cellSide>>0;
      return Game.toggleCellAndRedraw(x,y,function(gameState)
      {
-      return Canvas.drawLife(gameState);
+      return Canvas.drawLife(context,width,height,cellSide,gameState);
      });
     },
     canvas:Runtime.Field(function()
@@ -24,94 +24,82 @@
      _this=Canvas.element();
      return _this.Body;
     }),
-    canvasXOffset:Runtime.Field(function()
+    cellIsWithinViewableCanvas:function(_,_1,_2,_3)
     {
-     return 0;
-    }),
-    canvasYOffset:Runtime.Field(function()
-    {
-     return 0;
-    }),
-    cellIsWithinViewableCanvas:function(_,_1)
-    {
-     var canvasCoordinatePair;
-     canvasCoordinatePair=[_,_1];
-     if((canvasCoordinatePair[0]>0?canvasCoordinatePair[0]<Number(Canvas.canvas().width):false)?canvasCoordinatePair[1]>0:false)
-      {
-       return canvasCoordinatePair[1]<Number(Canvas.canvas().height);
-      }
-     else
-      {
-       return false;
-      }
+     return(Runtime.Tupled(function(canvasCoordinatePair)
+     {
+      if((canvasCoordinatePair[0]>0?canvasCoordinatePair[0]<Number(_):false)?canvasCoordinatePair[1]>0:false)
+       {
+        return canvasCoordinatePair[1]<Number(_1);
+       }
+      else
+       {
+        return false;
+       }
+     }))([_2,_3]);
     },
-    cellSide:Runtime.Field(function()
-    {
-     return 0;
-    }),
     context:Runtime.Field(function()
     {
      return Canvas.canvas().getContext("2d");
     }),
-    drawLife:function(gameState)
+    drawLife:function(context,width,height,cellSide,gameState)
     {
-     var x,x1,f,f2,f3;
-     Canvas.context().save();
-     Canvas.context().clearRect(0,0,Number(Canvas.canvas().width),Number(Canvas.canvas().height));
-     Canvas.context().fillStyle="#8EE5EE";
-     Canvas.context().fillRect(0,0,Number(Canvas.canvas().width),Number(Canvas.canvas().height));
-     Canvas.context().save();
-     Canvas.context().strokeStyle="black";
-     Canvas.context().fillStyle="black";
-     Canvas.context().lineWidth=1;
-     x=(x1=(f=function(set)
+     var x,x1,f,mapping,f1,predicate,f2,action;
+     context.save();
+     context.clearRect(0,0,Number(width),Number(height));
+     context.fillStyle="#8EE5EE";
+     context.fillRect(0,0,Number(width),Number(height));
+     context.save();
+     context.strokeStyle="black";
+     context.fillStyle="black";
+     context.lineWidth=1;
+     x=(x1=(f=(mapping=Runtime.Tupled(function(tupledArg)
      {
-      var f1,a,t;
-      f1=Runtime.Tupled(function(tupledArg)
-      {
-       var _arg00_,_arg01_;
-       _arg00_=tupledArg[0];
-       _arg01_=tupledArg[1];
-       return Canvas.transformCellCoordinatesToCanvasCoordinates(_arg00_,_arg01_);
-      });
-      a=Seq.map(f1,set);
+      var _arg10_,_arg11_;
+      _arg10_=tupledArg[0];
+      _arg11_=tupledArg[1];
+      return Canvas.transformCellCoordinatesToCanvasCoordinates(cellSide,_arg10_,_arg11_);
+     }),function(set)
+     {
+      var a,t;
+      a=Seq.map(mapping,set);
       t=BalancedTree.OfSeq(a);
       return _FSharpSet_1.New1(t);
-     },f(gameState)),(f2=function(set)
+     }),f(gameState)),(f1=(predicate=Runtime.Tupled(function(tupledArg)
      {
-      return SetModule.Filter(Runtime.Tupled(function(tupledArg)
-      {
-       var _arg00_,_arg01_;
-       _arg00_=tupledArg[0];
-       _arg01_=tupledArg[1];
-       return Canvas.cellIsWithinViewableCanvas(_arg00_,_arg01_);
-      }),set);
-     },f2(x1)));
-     f3=function(set)
+      var _arg20_,_arg21_;
+      _arg20_=tupledArg[0];
+      _arg21_=tupledArg[1];
+      return Canvas.cellIsWithinViewableCanvas(width,height,_arg20_,_arg21_);
+     }),function(set)
      {
-      var f1;
-      f1=Runtime.Tupled(function(tupledArg)
-      {
-       var _arg00_,_arg01_;
-       _arg00_=tupledArg[0];
-       _arg01_=tupledArg[1];
-       return Canvas.drawSmallCircleAtPoint(_arg00_,_arg01_);
-      });
-      return Seq.iter(f1,set);
-     };
-     f3(x);
-     return Canvas.context().save();
+      return SetModule.Filter(predicate,set);
+     }),f1(x1)));
+     f2=(action=Runtime.Tupled(function(tupledArg)
+     {
+      var _arg20_,_arg21_;
+      _arg20_=tupledArg[0];
+      _arg21_=tupledArg[1];
+      return Canvas.drawSmallCircleAtPoint(context,cellSide,_arg20_,_arg21_);
+     }),function(set)
+     {
+      return Seq.iter(action,set);
+     });
+     f2(x);
+     return context.save();
     },
-    drawSmallCircleAtPoint:function(_,_1)
+    drawSmallCircleAtPoint:function(_,_1,_2,_3)
     {
-     var canvasCoordinatePair,radius,endAngle;
-     canvasCoordinatePair=[_,_1];
-     radius=(Number(Canvas.cellSide())-2)/2;
-     endAngle=3.14159265358979*2;
-     Canvas.context().beginPath();
-     Canvas.context().arc(canvasCoordinatePair[0],canvasCoordinatePair[1],radius,0,endAngle,true);
-     Canvas.context().closePath();
-     return Canvas.context().stroke();
+     return(Runtime.Tupled(function(canvasCoordinatePair)
+     {
+      var radius,endAngle;
+      radius=(Number(_1)-2)/2;
+      endAngle=3.14159265358979*2;
+      _.beginPath();
+      _.arc(canvasCoordinatePair[0],canvasCoordinatePair[1],radius,0,endAngle,true);
+      _.closePath();
+      return _.stroke();
+     }))([_2,_3]);
     },
     element:Runtime.Field(function()
     {
@@ -122,7 +110,7 @@
      });
      return _this.NewTag("canvas",x);
     }),
-    initialize:function(width,height,cellSidePixels,canvasXOffsetPixels,canvasYOffsetPixels)
+    initialize:function(width,height,cellSide,canvasXOffset,canvasYOffset)
     {
      var target,_this;
      if(Unchecked.Equals((target=Canvas.canvas(),target.getContext),undefined))
@@ -131,23 +119,11 @@
       }
      Canvas.canvas().width=width;
      Canvas.canvas().height=height;
-     Canvas.cellSide=function()
-     {
-      return cellSidePixels;
-     };
-     Canvas.canvasXOffset=function()
-     {
-      return canvasXOffsetPixels;
-     };
-     Canvas.canvasYOffset=function()
-     {
-      return canvasYOffsetPixels;
-     };
      Canvas.context().canvas.addEventListener("click",function(evt)
      {
-      return Canvas.adjustInitArrayAndRedraw(evt);
+      return Canvas.adjustInitArrayAndRedraw(Canvas.context(),width,height,cellSide,canvasXOffset,canvasYOffset,evt);
      },false);
-     Canvas.drawLife(Game.currentState().contents);
+     Canvas.drawLife(Canvas.context(),width,height,cellSide,Game.currentState().contents);
      return Util.addListener((_this=Canvas.GameEvents(),_this.event),function(_arg1)
      {
       if(_arg1.$==1)
@@ -160,14 +136,14 @@
          {
           return Game.stopDrawingAndReset(function(gameState)
           {
-           return Canvas.drawLife(gameState);
+           return Canvas.drawLife(Canvas.context(),width,height,cellSide,gameState);
           });
          }
         else
          {
           return Game.startDrawing(function(gameState)
           {
-           return Canvas.drawLife(gameState);
+           return Canvas.drawLife(Canvas.context(),width,height,cellSide,gameState);
           });
          }
        }
@@ -178,19 +154,21 @@
      var _this,_this1;
      return Operators.add(Default.Div(List.ofArray([Default.Width(Global.String(Canvas.canvas().width)),(_this=Default.Attr(),_this.NewAttr("style","float:left; clear:both"))])),List.ofArray([Operators.add(Default.Div(List.ofArray([(_this1=Default.Attr(),_this1.NewAttr("style","float:center"))])),List.ofArray([Canvas.element()]))]));
     },
-    transformCellCoordinatesToCanvasCoordinates:function(_,_1)
+    transformCellCoordinatesToCanvasCoordinates:function(_,_1,_2)
     {
-     var cell,centerX,x,f,centerY,x1,f1;
-     cell=[_,_1];
-     centerX=(x=cell[0]*Canvas.cellSide()+(Canvas.cellSide()/2>>0),(f=function(value)
+     return(Runtime.Tupled(function(cell)
      {
-      return Number(value);
-     },f(x)));
-     centerY=(x1=cell[1]*Canvas.cellSide()+(Canvas.cellSide()/2>>0),(f1=function(value)
-     {
-      return Number(value);
-     },f1(x1)));
-     return[centerX,centerY];
+      var centerX,x,f,centerY,x1,f1;
+      centerX=(x=cell[0]*_+(_/2>>0),(f=function(value)
+      {
+       return Number(value);
+      },f(x)));
+      centerY=(x1=cell[1]*_+(_/2>>0),(f1=function(value)
+      {
+       return Number(value);
+      },f1(x1)));
+      return[centerX,centerY];
+     }))([_1,_2]);
     }
    }
   }
@@ -200,10 +178,10 @@
   WebSharper=Runtime.Safe(Global.IntelliFactory.WebSharper);
   Control=Runtime.Safe(WebSharper.Control);
   _FSharpEvent_1=Runtime.Safe(Control["FSharpEvent`1"]);
-  LifeHtml=Runtime.Safe(Global.LifeHtml);
-  Canvas=Runtime.Safe(LifeHtml.Canvas);
   Life=Runtime.Safe(Global.Life);
   Game=Runtime.Safe(Life.Game);
+  LifeHtml=Runtime.Safe(Global.LifeHtml);
+  Canvas=Runtime.Safe(LifeHtml.Canvas);
   Number=Runtime.Safe(Global.Number);
   Seq=Runtime.Safe(WebSharper.Seq);
   Collections=Runtime.Safe(WebSharper.Collections);
@@ -224,9 +202,6 @@
  {
   Canvas.element();
   Canvas.context();
-  Canvas.cellSide();
-  Canvas.canvasYOffset();
-  Canvas.canvasXOffset();
   Canvas.canvas();
   Canvas.GameEvents();
  });
